2a2630e1d285b898432d37433ab80b2d5a2a4da6
as.kbourrie <as.kbourrie@gmail.com>
2011-01-25 18:04:46 +0000

    * mise à niveau, phase de mise en place des parcours /2

--- src/org/jredway/model/BaseParcours.java
+++ src/org/jredway/model/BaseParcours.java
@@ -2,10 +2,17 @@ package org.jredway.model;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
+
+import org.jredway.openscore.OpenScoreServlet;
+
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Classe permettant de gèrer la classe Parcours
@@ -14,8 +21,10 @@ import java.util.List;
  * @author killian.b
  * @version 1.0.0
  */
+@SuppressWarnings("unused")
 public class BaseParcours {
 
+    private static final Logger log = Logger.getLogger(OpenScoreServlet.class.getName());
     private Parcours parcours;
     private UserService userService = UserServiceFactory.getUserService();
     private User user = userService.getCurrentUser();
@@ -40,14 +49,32 @@ public class BaseParcours {
      * @param index
      * @since 1.0.0
      */
-    public void ajouter(User compte, String nomParours, String depart, int trou[]){
+    public void ajouter(User compte, String nomParours, String depart, ArrayList<Integer> trou, Date date){
         PersistenceManager pm = PMF.get().getPersistenceManager();
-        this.parcours = new Parcours(compte, nomParours, depart, trou);
+        this.parcours = new Parcours(compte, nomParours, depart, trou, date);
         try {
             pm.makePersistent(parcours);
         } finally {
             pm.close();
         }
+    }
+    
+    /**
+     * Méthode qui supprime un parcours
+     * dans la bdd
+     * 
+     * @see PMF
+     * @param compte
+     * @param date
+     * @since 1.0.0
+     */
+    public void supprimer(User compte, Date date) {
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        Query query = pm.newQuery(Parcours.class);
+        query.declareImports("import com.google.appengine.api.users.User; import java.util.Date;");
+        query.setFilter("compte == pCompte && dateParcours == pDate");
+        query.declareParameters("User pCompte, Date pDate");
+        query.deletePersistentAll(compte, date);
     }
     
 }
